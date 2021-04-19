7a00d6c219e11b9161233e21448dbe7ab2c9f362
John Janzen <78975274+xjanzen@users.noreply.github.com>
2021-04-18 22:14:18 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -25,3 +25,17 @@ GROUP BY title
 ORDER BY count DESC
 
 SELECT * FROM retiring_titles
+
+-- Create a Mentorship Eligibility table for current employees who were born between January 1, 1965 and December 31, 1965
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, d.from_date, d.to_date, ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as d
+ON e.emp_no = d.emp_no
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (d.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility
